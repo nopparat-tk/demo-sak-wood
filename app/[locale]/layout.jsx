0bf0eb0353cb5ed6568dce1c73f17f5c3a0650a2
nopparat-tk <105415402+nopparat-tk@users.noreply.github.com>
2025-07-31 16:06:20 +0700

    Update Yandex Metrika counter

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -27,6 +27,32 @@ export default async function LocaleLayout({ children, params }) {
       lang={locale}
       className={`${figtree.variable} ${barlowSemiCondensed.variable}`}
     >
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103455031', 'ym');
+
+              ym(103455031, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/103455031"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </head>
       <body>
         <GoogleTagManager gtmId="GTM-KHH72PKH" />
         <NextIntlClientProvider locale={locale}>
