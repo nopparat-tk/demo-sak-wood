f05f836af080413469ea7391ff851894dbde02fe
nopparat-tk <105415402+nopparat-tk@users.noreply.github.com>
2025-07-31 14:51:37 +0700

    Update GTM-id

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -9,6 +9,8 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 
+import { GoogleTagManager } from "@next/third-parties/google";
+
 export const metadata = {
   title: "SAK Wood",
   description: "Curated selection of meticulously graded premium hardwoods",
@@ -25,32 +27,8 @@ export default async function LocaleLayout({ children, params }) {
       lang={locale}
       className={`${figtree.variable} ${barlowSemiCondensed.variable}`}
     >
-      <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KHH72PKH');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager */}
-      </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KHH72PKH"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        <GoogleTagManager gtmId="	GTM-KHH72PKH" />
         <NextIntlClientProvider locale={locale}>
           {children}
         </NextIntlClientProvider>
