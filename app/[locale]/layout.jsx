0ff26a07c82f089520109aca586bf7c833298925
nopparat-tk <105415402+nopparat-tk@users.noreply.github.com>
2025-07-31 13:47:39 +0700

    update Google Tag Manager & Breadcrumb

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -25,7 +25,32 @@ export default async function LocaleLayout({ children, params }) {
       lang={locale}
       className={`${figtree.variable} ${barlowSemiCondensed.variable}`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KHH72PKH');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KHH72PKH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <NextIntlClientProvider locale={locale}>
           {children}
         </NextIntlClientProvider>
